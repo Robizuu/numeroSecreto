71cb69b016be46f54fb17fefe1ab510d3240c437
Gabriel Motta Martins <121207690+Robizuu@users.noreply.github.com>
2024-11-14 18:38:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+alert('O Guga é viado');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 40');
+
+if (chute == numeroSecreto) {
+    alert(`Isso aí! Voce descobriu o numero secreto ${numeroSecreto}`);
+} else {
+    alert('Faz o L')
+}
+
